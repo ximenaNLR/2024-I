c873dc079907c499cdaed78b9410b839c6a60054
sebastian <juans.rivasquemba@gmail.com>
2024-05-10 12:45:45 -0500

    suma de variables a y b

---
+++ Git/src/git/Git.java
@@ -0,0 +1,15 @@
+
+package git;
+
+
+public class Git {
+
+    public static void main(String[] args) {
+        System.out.println(" ");
+        int a = 4;
+        int b = 6;
+        int total = a+b;
+        System.out.println(total);
+    }
+    
+}
